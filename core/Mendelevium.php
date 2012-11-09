f874a05e8cb730d6b17228a80f4d3fe7d5a7eba2
Felipe de Souza <de.souza258@gmail.com>
2012-11-08 23:18:20 -0200

    Generic Logic And More
    Generic Logic , Some PHP errors and example in web interface.

--- core/Mendelevium.php
+++ core/Mendelevium.php
@@ -6,21 +6,58 @@
 	class Mendelevium
 	{
 
-		private $_redisScheme = 'Mendelevium';
+		/* Redis Configuration Info */
+
+		private $_redisScheme = 'tcp';
 		private $_redisHost = 'localhost';
 		private $_redisPort = 6379;
+		public $redis;
 
-		function __construct(){
-			$redis = new Predis\Client(array(
-   			 'scheme' =>  $this->_redisScheme,
-    		 'host'   => $this->_redisHost,
-    		 'port'   => $this->_redisPort,
-			));
 
+		/* Integer Var used as helper so I can discover how many logs really exists in memory */
+		public static $num_helper;
+
+		/* Project Name , used by Melenevium for creating beauty semantic internal log getters */
+		public $projName;
+
+
+		function __construct($_projName){
+			try {
+				$this->redis = new Predis\Client(array(
+	   			 'scheme' =>  $this->_redisScheme,
+	    		 'host'   => $this->_redisHost,
+	    		 'port'   => $this->_redisPort,
+				));
+
+				if(!$this->redis->exists('MendeleviumEC')){
+					$this->redis->set("MendeleviumEC",0);
+					$this->num_helper = $this->redis->get("MendeleviumEC");
+				}else{
+					$this->num_helper = $this->redis->get("MendeleviumEC");
+				}
+
+
+				$this->projName = $_projName;
+
+			}
+			catch (Exception $e) {
+			    echo "Couldn't connected to Redis";
+			    echo $e->getMessage();
+			}
 		}
 
-		function log(){
-
+		function log($message = NULL){
+			try {
+				if(is_null($message) || trim($message) == '') throw new Exception("Invalid Log Message");
+				else{
+					$this->redis->set($this->projName.'LogN'.$this->num_helper,$message);
+					if($this->redis->incr('MendeleviumEC')){
+						$this->num_helper++;
+					} else throw new Exception("Can't Elevate Internal Helper. \n It Should Never Get This Error Since Logic is Perfect!");
+				}
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
 		}
 
 	}
--- interface/index.php
+++ interface/index.php
@@ -23,15 +23,14 @@
 
     <div class="body">
 
-    	<p>
-    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    	</p>
-
+    	<?php
+    	/*
+    		require '../core/Mendelevium.php';
+    		$m = new Mendelevium('lifeGood');
+    		$m->log("arroz");
+    		echo $m->redis->get('lifeGoodLogN0');
+    	*/
+    	 ?>
 
     </div>
 <footer>
