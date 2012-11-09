e1904103893082aa0338ee1542fe0dfae450448d
Felipe de Souza <de.souza258@gmail.com>
2012-11-08 22:21:19 -0200

    Core da Web Interface

--- interface/index.php
+++ interface/index.php
@@ -1,10 +1,44 @@
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title></title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Mendelevium - Web Interface</title>
+    <link rel="stylesheet" href="static/css/reset.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="static/css/main.css" type="text/css" media="screen" charset="utf-8">
 </head>
 <body>
+    <header>
+        <div class="container">
+            <a href="/" alt="overview"><h1>Md - log php engine</h1></a>
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="/" alt="overview">overview</a></li>
+                        <li><a href="/" alt="about">about</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <div class="body">
+
+    	<p>
+    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    	</p>
+
+
+    </div>
+<footer>
+        <div class="cell">
+            <a href="http://github.com/inazagi/mendelevium" alt="Mendelevium, a PHP Log Engine">powered by Mendelevium</a>
+        </div>
+</footer>
 
 </body>
 </html>
